1eee8350db963c54f01861564789c6cb0f88dab6
hoergems <hoergems@gmail.com>
2016-10-27 10:28:41 +1000

    Empty

---
+++ WAFRExtension/emptyEnvironmentABTHFR/manipulator/jobs_abt_manipulator.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#SBATCH --job-name=abtMan
+#SBATCH --array=1-4
+#SBATCH --time=10:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=4096
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hoergems@gmail.com
+
+source /home/hoe01h/.bash_profile
+cd /data/hoe01h/WAFRExtension/abt/problems/robot_problem
+./abt_manipulator --cfg data/hoe01h/WAFRExtension/ConfigFiles/WAFRExtension/emptyEnvironmentABTHFR/manipulator/manipulator4DOFEmpty$SLURM_ARRAY_TASK_ID.cfg
