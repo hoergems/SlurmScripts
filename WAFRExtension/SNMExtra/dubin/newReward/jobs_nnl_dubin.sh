c1d2e5f6f5e3f5cbfea9be1f1b7e972ef54aa0af
hoergems <hoergems@gmail.com>
2016-12-10 19:40:58 +1000

    SNM

---
+++ WAFRExtension/SNMExtra/dubin/newReward/jobs_nnl_dubin.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+#SBATCH --job-name=EnnlDubin
+#SBATCH --time=05:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=16000
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=hoergems@gmail.com
+
+source /home/hoe01h/.bash_profile
+cd /data/hoe01h/WAFRExtension/abt3/problems/robot_problem
+./nnl_dubin --cfg /data/hoe01h/WAFRExtension/ConfigFiles/WAFRExtension/SNMExtra/dubin/newReward/dubin.cfg
