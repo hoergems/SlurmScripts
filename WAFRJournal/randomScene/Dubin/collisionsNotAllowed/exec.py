1ec6903ad09fcfbed43bafdf6ba47abeee928e34
hoergems <hoergems@gmail.com>
2017-03-15 17:15:13 +1000

    Added exec script

---
+++ WAFRJournal/randomScene/Dubin/collisionsNotAllowed/exec.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import subprocess
+import glob
+
+shared_path = os.path.dirname(os.path.abspath(__file__))
+numObstacles = 0
+
+for i in xrange(6):
+    numObstacles += 5
+    for j in xrange(1, 11):
+	cmd = "sbatch jobs_abt_dubin_" + str(j) + ".sh"
+	path = str(numObstacles) + "_obstacles/" + str(j) + "/"
+	print path
+	print cmd
+	popen = subprocess.Popen(cmd, cwd=path)
+	popen.wait()
+print "launched \n"
