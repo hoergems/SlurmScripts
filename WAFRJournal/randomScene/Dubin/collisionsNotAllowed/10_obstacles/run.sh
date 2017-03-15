517149ca96edbe794a21fdd470b623073296f556
hoergems <hoergems@gmail.com>
2017-03-15 21:36:46 +1000

    Fix

--- WAFRJournal/randomScene/Dubin/collisionsNotAllowed/10_obstacles/run.sh
+++ WAFRJournal/randomScene/Dubin/collisionsNotAllowed/10_obstacles/run.sh
@@ -1,13 +1,19 @@
 #! /bin/bash
 
+unset output
+
 for i in $(seq 10)
 do
   cd $i
-  jid=$(sbatch jobs_abt_dubin_$j.sh)
   for ((a=1; a <= 10 ; a++))
   do
-    jid=$(sbatch --dependency=afterany:$jid jobs_abt_dubin_$a.sh)
-    echo $?
-  done  
+    if [ -z "$output" ]
+    then
+      output=$(sbatch jobs_abt_dubin_$a.sh)      
+    else
+      output=$(sbatch --dependency=afterok:$jid jobs_abt_dubin_$a.sh)
+    fi
+    jid=$(echo $output | tr -cd '[[:digit:]]')
+  done
   cd ..
 done
