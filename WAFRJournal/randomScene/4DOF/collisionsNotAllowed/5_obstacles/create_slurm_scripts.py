c6ee4576495b83248933e6ba14cd68dadb5c4c20
hoergems <hoergems@gmail.com>
2017-03-28 16:53:30 +1000

    Fixes

--- WAFRJournal/randomScene/4DOF/collisionsNotAllowed/5_obstacles/create_slurm_scripts.py
+++ WAFRJournal/randomScene/4DOF/collisionsNotAllowed/5_obstacles/create_slurm_scripts.py
@@ -15,20 +15,21 @@ args = parser.parse_args()
 
 numObstacles = args.numObstacles
 numParallelJobs = args.numParallelJobs
-robot = args.robot
-robotExecName = "dubin"
-if robot == "4DOF":
-    robotExecName = "robot"
 
+robot = args.robot
+robotExec = "dubin"
+if (robot == "4DOF"):
+    robotExec = "robot"
 
 shared_path = os.path.dirname(os.path.abspath(__file__))
 
+# Create the scripts for ABT
 for i in xrange(1, 11):
     for j in xrange(0, 100/numParallelJobs):
         string = "#!/bin/sh \n"
         string += "# \n"
         string += "#SBATCH --job-name=" + str(numObstacles)
-        string += "4DOFABT \n"
+        string += robot + "ABT \n"
         string += "#SBATCH --array="
         string += str(j * numParallelJobs) + "-" + str(j * numParallelJobs + numParallelJobs-1) + " \n"
         string += "#SBATCH --time=00:20:00 \n"
@@ -39,16 +40,48 @@ for i in xrange(1, 11):
         string += "#SBATCH --mail-user=hoergems@gmail.com \n"
         string += "source /home/hoe01h/.bash_profile \n"
         string += "cd /data/hoe01h/Downloads/frapu/abt/bin \n"
-        string += "./abt_" + robotExecName + " --cfg /data/hoe01h/Downloads/frapu/ConfigFiles/WAFRJournal/randomScene/" + robot + "/collisionsNotAllowed/"
+        string += "./abt_" + robotExec + " --cfg /data/hoe01h/Downloads/frapu/ConfigFiles/WAFRJournal/randomScene/" + robot + "/collisionsNotAllowed/"
+        string += str(numObstacles)
+        string += "_obstacles/cfg/"
+        string += str(i)
+        string += "/" + robot + "$SLURM_ARRAY_TASK_ID.cfg \n"        
+        if not os.path.exists(str(i)):
+	    os.makedirs(str(i))
+	if (os.path.exists(str(i) + "/jobs_abt_" + robot + "_" + str(j) + ".sh")):
+	    os.remove(str(i) + "/jobs_abt_" + robot + "_" + str(j) + ".sh")
+	with open(str(i) + "/jobs_abt_" + robot + "_" + str(j) + ".sh", 'a+') as f:
+	    f.write(string)
+	  
+#Create the scripts for MHFR
+for i in xrange(1, 11):
+    for j in xrange(0, 100/numParallelJobs):
+	string = "#!/bin/sh \n"
+        string += "# \n"
+        string += "#SBATCH --job-name=" + str(numObstacles)
+        string += robot + "MHFR \n"
+        string += "#SBATCH --array="
+        string += str(j * numParallelJobs) + "-" + str(j * numParallelJobs + numParallelJobs-1) + " \n"        
+        string += "#SBATCH --time=00:20:00 \n"
+        string += "#SBATCH --nodes=1 \n"
+        string += "#SBATCH --ntasks=1 \n"
+        string += "#SBATCH --cpus-per-task=8 \n"
+        string += "#SBATCH --mem=4096 \n"
+        string += "#SBATCH --mail-type=ALL \n"
+        string += "#SBATCH --mail-user=hoergems@gmail.com \n"
+        string += "source /home/hoe01h/.bash_profile \n"
+        string += "cd /data/hoe01h/Downloads/frapu/abt/bin \n"
+        string += "./hfr_" + robotExec + " --cfg /data/hoe01h/Downloads/frapu/ConfigFiles/WAFRJournal/randomScene/" + robot + "/collisionsNotAllowed/"
         string += str(numObstacles)
         string += "_obstacles/cfg/"
         string += str(i)
         string += "/" + robot + "$SLURM_ARRAY_TASK_ID.cfg \n"
-        print string
         if not os.path.exists(str(i)):
 	    os.makedirs(str(i))
-	with open(str(i) + "/jobs_abt_" + robot + "_" + str(j) + ".sh", 'a+') as f:
+	if (os.path.exists(str(i) + "/jobs_mhfr_" + robot + "_" + str(j) + ".sh")):
+	    os.remove(str(i) + "/jobs_mhfr_" + robot + "_" + str(j) + ".sh")
+	with open(str(i) + "/jobs_mhfr_" + robot + "_" + str(j) + ".sh", 'a+') as f:
 	    f.write(string)
+        
 	
 with open("run.sh", 'a+') as f:
     for line in f.readlines():
