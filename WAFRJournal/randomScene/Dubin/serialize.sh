9ae096c97f883728102d1d634ed8fc840aa549a2
hoergems <hoergems@gmail.com>
2017-04-06 08:01:25 +1000

    Fixes

--- WAFRJournal/randomScene/Dubin/serialize.sh
+++ WAFRJournal/randomScene/Dubin/serialize.sh
@@ -7,6 +7,6 @@ for NUM_OBSTACLES in 5 10 15 20 25 30
 do   
    for FOLDER in {1..10}
    do
-       ./statsSerializer -p /datastore/hoe01h/WAFRJournal/randomScene/4DOF/collisionsNotAllowed/${NUM_OBSTACLES}_obstacles/$FOLDER
+       ./statsSerializer -p /datastore/hoe01h/WAFRJournal/randomScene/Dubin/collisionsNotAllowed/${NUM_OBSTACLES}_obstacles/$FOLDER
    done
 done
