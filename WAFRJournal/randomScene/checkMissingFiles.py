aec6b93e8ce75304855b6c6d0febdd535d8c21a2
hoergems <hoergems@gmail.com>
2017-04-18 19:00:57 +1000

    Check

--- WAFRJournal/randomScene/checkMissingFiles.py
+++ WAFRJournal/randomScene/checkMissingFiles.py
@@ -18,6 +18,11 @@ shared_path = os.path.dirname(os.path.abspath(__file__))
 
 for numObstacles in [5, 10, 15, 20, 25]:
     for folder in xrange(1, 11):
-	path = robot + "/" + str(numObstacles) + "_obstacles/" + str(folder)
-	for file in glob.glob("*.log"):
+	path = "/datastore/hoe01h/WAFRJournal/randomScene/" + robot + "/collisionsNotAllowed/" + str(numObstacles) + "_obstacles/" + str(folder)
+	files = glob.glob(path + "/*.log")
+	for file in files:
 	    print file
+	'''for count in xrange(1, 101):
+	    
+	for file in glob.glob("*.log"):
+	    print file'''
