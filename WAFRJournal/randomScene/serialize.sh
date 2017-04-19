bb68b7318dae9b2aa8afff595c53a4e9ca91f861
hoergems <hoergems@gmail.com>
2017-04-19 12:58:55 +1000

    Improvements in serialization script

--- WAFRJournal/randomScene/serialize.sh
+++ WAFRJournal/randomScene/serialize.sh
@@ -1,12 +1,19 @@
 #! /bin/bash
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+if [ -z $1 ]
+then
+    echo "No robot provided"
+    exit    
+fi
+
+robot=$1
 
 cd /data/hoe01h/Downloads/frapu/abt_stats/abt/bin
 for NUM_OBSTACLES in 5 10 15 20 25 30 
 do   
    for FOLDER in {1..10}
    do
-       ./statsSerializer -p /datastore/hoe01h/WAFRJournal/randomScene/Dubin/collisionsNotAllowed/${NUM_OBSTACLES}_obstacles/$FOLDER
+       ./statsSerializer -p /datastore/hoe01h/WAFRJournal/randomScene/${robot}/collisionsNotAllowed/${NUM_OBSTACLES}_obstacles/$FOLDER
    done
 done
