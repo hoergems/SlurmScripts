659d1c8591b881b2686acc2485ccfd1d437c972e
hoergems <hoergems@gmail.com>
2017-07-17 16:14:32 +1000

    Fix

--- WAFRJournal/largeErrors/4DOFFactory1/create_slurm_scripts.py
+++ WAFRJournal/largeErrors/4DOFFactory1/create_slurm_scripts.py
@@ -56,8 +56,9 @@ for i in xrange(1, numConvarianceSteps + 1):
 	    string += "#SBATCH --mail-type=END,FAIL \n"
 	    string += "#SBATCH --mail-user=hoergems@gmail.com \n"
 	    string += "source /home/hoe01h/.bash_profile \n"
-	    string += "gzMasterUri=`expr 11345 + $SLURM_ARRAY_TASK_ID` \n"
-	    string += "export GAZEBO_MASTER_URI=http://localhost:$gzMasterUri \n"
+	    string += "gzMasterUriPort=`expr 11345 + $SLURM_ARRAY_TASK_ID` \n"
+	    string += "echo $gzMasterUriPort \n"
+	    string += "export GAZEBO_MASTER_URI=http://localhost:$gzMasterUriPort \n"
 	    string += "cd /data/hoe01h/tapir_devel/bin \n"
 	    string += "./abt_robot --cfg /data/hoe01h/ConfigFiles/WAFRJournal/largeErrors/4DOFFactory1/cfg/" + folder2	    
 	    string += "/4DOFFactory1_$SLURM_ARRAY_TASK_ID.cfg \n"        
@@ -87,8 +88,9 @@ for i in xrange(1, numConvarianceSteps + 1):
 	    string += "#SBATCH --mail-type=END,FAIL \n"
 	    string += "#SBATCH --mail-user=hoergems@gmail.com \n"
 	    string += "source /home/hoe01h/.bash_profile \n"
-	    string += "gzMasterUri=`expr 11345 + $SLURM_ARRAY_TASK_ID` \n"
-	    string += "export GAZEBO_MASTER_URI=http://localhost:$gzMasterUri \n"
+	    string += "gzMasterUriPort=`expr 11345 + $SLURM_ARRAY_TASK_ID` \n"
+	    string += "echo $gzMasterUriPort \n"
+	    string += "export GAZEBO_MASTER_URI=http://localhost:$gzMasterUriPort \n"
 	    string += "cd /data/hoe01h/tapir_devel/bin \n"
 	    string += "./mhfr_robot --cfg /data/hoe01h/ConfigFiles/WAFRJournal/largeErrors/4DOFFactory1/cfg/" + folder2	    
 	    string += "/4DOFFactory1_$SLURM_ARRAY_TASK_ID.cfg \n"        
