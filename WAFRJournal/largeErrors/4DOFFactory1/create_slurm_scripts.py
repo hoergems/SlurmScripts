1b65043c1eb81edf133eee771f2011cdab0a4e27
hoergems <hoergems@gmail.com>
2017-07-17 15:58:24 +1000

    Explicitly set the gazebo master uri in slurm scripts

--- WAFRJournal/largeErrors/4DOFFactory1/create_slurm_scripts.py
+++ WAFRJournal/largeErrors/4DOFFactory1/create_slurm_scripts.py
@@ -41,8 +41,9 @@ os.makedirs(folder)
 # Create the scripts for ABT
 for i in xrange(1, numConvarianceSteps + 1):
     for j in xrange(1, numConvarianceSteps + 1):
-	folder2 = str(i) + "_proc_" + str(j) + "_obs"
+	folder2 = str(i) + "_proc_" + str(j) + "_obs"	
 	for k in xrange(0, numRuns/numParallelJobs):
+	    gzMasterUri = 11345 + k
 	    string = "#!/bin/sh \n"
 	    string += "# \n"
 	    string += "#SBATCH --job-name=" + str(numObstacles)
@@ -56,6 +57,7 @@ for i in xrange(1, numConvarianceSteps + 1):
 	    string += "#SBATCH --mail-type=END,FAIL \n"
 	    string += "#SBATCH --mail-user=hoergems@gmail.com \n"
 	    string += "source /home/hoe01h/.bash_profile \n"
+	    string += "export GAZEBO_MASTER_URI=http://localhost:" + str(gzMasterUri)
 	    string += "cd /data/hoe01h/tapir_devel/bin \n"
 	    string += "./abt_robot --cfg /data/hoe01h/ConfigFiles/WAFRJournal/largeErrors/4DOFFactory1/cfg/" + folder2	    
 	    string += "/4DOFFactory1_$SLURM_ARRAY_TASK_ID.cfg \n"        
@@ -71,6 +73,7 @@ for i in xrange(1, numConvarianceSteps + 1):
     for j in xrange(1, numConvarianceSteps + 1):
 	folder2 = str(i) + "_proc_" + str(j) + "_obs"
 	for k in xrange(0, numRuns/numParallelJobs):
+	    gzMasterUri = 11345 + k
 	    string = "#!/bin/sh \n"
 	    string += "# \n"
 	    string += "#SBATCH --job-name=" + str(numObstacles)
@@ -85,6 +88,7 @@ for i in xrange(1, numConvarianceSteps + 1):
 	    string += "#SBATCH --mail-type=END,FAIL \n"
 	    string += "#SBATCH --mail-user=hoergems@gmail.com \n"
 	    string += "source /home/hoe01h/.bash_profile \n"
+	    string += "export GAZEBO_MASTER_URI=http://localhost:" + str(gzMasterUri)
 	    string += "cd /data/hoe01h/tapir_devel/bin \n"
 	    string += "./mhfr_robot --cfg /data/hoe01h/ConfigFiles/WAFRJournal/largeErrors/4DOFFactory1/cfg/" + folder2	    
 	    string += "/4DOFFactory1_$SLURM_ARRAY_TASK_ID.cfg \n"        
