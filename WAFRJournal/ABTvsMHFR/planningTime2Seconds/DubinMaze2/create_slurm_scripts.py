62fb15652a8960b983ec1a2110bd8f15288ed904
hoergems <hoergems@gmail.com>
2017-11-24 18:09:55 +1000

    Update

--- WAFRJournal/ABTvsMHFR/planningTime2Seconds/DubinMaze2/create_slurm_scripts.py
+++ WAFRJournal/ABTvsMHFR/planningTime2Seconds/DubinMaze2/create_slurm_scripts.py
@@ -61,7 +61,7 @@ for i in xrange(1, numConvarianceSteps + 1):
 	    string += "gzMasterUriPort=`expr 11345 + $SLURM_ARRAY_TASK_ID` \n"
 	    string += "echo $gzMasterUriPort \n"
 	    string += "export GAZEBO_MASTER_URI=http://localhost:$gzMasterUriPort \n"
-	    string += "cd /data/hoe01h/tapir_devel/bin \n"
+	    string += "cd /data/hoe01h/oppt_devel/bin \n"
 	    string += "./abt --cfg " + configFolder + robot + "/cfg/" + folder2
 	    string += "/" + robot + "_$SLURM_ARRAY_TASK_ID.cfg \n"	     
 	    if not os.path.exists(folder + "/" + str(i) + "_proc_" + str(j) + "_obs"):
