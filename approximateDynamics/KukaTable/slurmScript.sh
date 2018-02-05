3eb9f81e3b76193a241050fe1218e92f35b247a0
hoergems <hoergems@gmail.com>
2018-02-05 11:54:24 +1000

    Add

---
+++ approximateDynamics/KukaTable/slurmScript.sh
@@ -0,0 +1,19 @@
+#!/bin/sh 
+# 
+#SBATCH --job-name=calcHeuristicSamples
+#SBATCH --array=0-999 
+#SBATCH --time=00:25:00 
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --cpus-per-task=7 
+#SBATCH --mem=12000 
+#SBATCH --mail-type=NONE 
+#SBATCH --mail-user=hoergems@gmail.com 
+source /home/hoe01h/.bash_profile 
+gzMasterUriPort=`expr 11345 + $SLURM_ARRAY_TASK_ID` 
+echo $gzMasterUriPort 
+export GAZEBO_MASTER_URI=http://localhost:$gzMasterUriPort 
+export OPPT_RESOURCE_PATH=$OPPT_RESOURCE_PATH:/data/hoe01h/oppt_devel/files/ 
+export OPPT_RESOURCE_PATH=$OPPT_RESOURCE_PATH:/data/hoe01h/gazebo_models/models/randomScenes/Dubin 
+cd /data/hoe01h/oppt_devel/bin 
+./calcHeuristicSamples --cfg /flush1/hoe01h/ConfigFiles/approximateDynamics/KukaTable/KukaTable$SLURM_ARRAY_TASK_ID.cfg
