4652be1950e642aa1d13e050f8d2ec301a4841b1
hoergems <hoergems@gmail.com>
2018-06-19 12:18:10 +1000

    add

--- ObservationSplitting/resultParser.py
+++ ObservationSplitting/resultParser.py
@@ -1,17 +1,19 @@
-import os
+import os.path
 import math
 
-for i in xrange(1, 11):
+for i in xrange(1, 51):
 	succRuns = 0
 	rewards = []
-	for j in xrange(25):
-		with open(str(i) + "_dist/log_ABT_4DOFManipulator_" + str(j) + ".log", 'r') as f:
-			for line in f.readlines():
-				if "Num successful runs" in line:
-					if "Num successful runs: 1" in line:
-						succRuns = succRuns + 1
-				elif "Mean rewards:" in line:
-					rewards.append(float(line.split(": ")[1].rstrip()))
+	for j in xrange(50):
+		logFile = str(i) + "_dist/log_ABT_4DOFManipulator_" + str(j) + ".log", 'r'
+		if os.path.isfile(logfile):
+			with open(logFile, 'r') as f:
+				for line in f.readlines():
+					if "Num successful runs" in line:
+						if "Num successful runs: 1" in line:
+							succRuns = succRuns + 1
+					elif "Mean rewards:" in line:
+						rewards.append(float(line.split(": ")[1].rstrip()))
 	summ = 0.0
 	for k in xrange(len(rewards)):
 		summ = summ + rewards[k]
