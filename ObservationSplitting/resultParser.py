6e7e56ae4614075306db6dbc61c6634ccb9f5a85
hoergems <hoergems@gmail.com>
2018-06-19 11:45:25 +1000

    add

--- ObservationSplitting/resultParser.py
+++ ObservationSplitting/resultParser.py
@@ -11,5 +11,5 @@ for i in xrange(1, 11):
 						succRuns = succRuns + 1
 				elif "Mean rewards:" in line:
 					rewards.append(float(line.split(": ")[1].rstrip()))
-    std = sqrt(2.0)
+	std = sqrt(2.0)
 	print "succRuns: " + str(succRuns)
