45bb38d3c2a861acf0d1f3c1e518808a6e05da9d
hoergems <hoergems@gmail.com>
2018-06-19 11:37:40 +1000

    add

--- ObservationSplitting/resultParser.py
+++ ObservationSplitting/resultParser.py
@@ -1,7 +1,12 @@
 import os
 
 for i in xrange(1, 11):
+	succRuns = 0
 	for j in xrange(25):
 		with open(str(i) + "_dist/log_ABT_4DOFManipulator_" + str(j) + ".log", 'r') as f:
 			for line in f.readlines():
-				print line
+				if "Num successful runs" in line:
+					if "Num successful runs: 1":
+						succRuns = succRuns + 1
+				elif "Mean rewards:" in line:
+					print line.split(": ")[1]
